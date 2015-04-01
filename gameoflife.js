269aa80b961fd0a1cc77d9263addd2412fed20bd
dmt262 <dthelen@ky-rafting.com>
2015-03-31 22:22:36 -0400

    clean up code, add comments

--- gameoflife.js
+++ gameoflife.js
@@ -2,13 +2,17 @@
 // This file contains the actual board array
 // and all the logic to set and/or evolve the board
 
+// All work on this object is performaed on the cells[].
+// No display is implied.  
+// This is the M in an MVC model
 
 var gameoflife = {
-  cells: [],
-  numSteps: 0,
-  population: 0,
+  cells: [],  // array to store all the live/dead cell positions
+  numSteps: 0,  // keeps track of the number of steps run through so far.
+  population: 0,  // Keeps the current # of live cells
 
-  setBoardSize: function(n) {
+  // creates an n x n array to store 0/1 = dead/live cells
+  setBoardSize: function(n) {  
     var i,j;
 
     this.cells.push([]);
@@ -21,6 +25,10 @@ var gameoflife = {
     this.numSteps = 0;
   },
 
+
+
+  // Once a board is initialized, this method will fill it in with pct percent chance
+  // of a cell being alive (1)
 
   setRandomBoard: function(pct) {
     var i,j;
@@ -38,6 +46,17 @@ var gameoflife = {
     this.numSteps = 0;
   },
 
+  // Implements the following rules:
+  //
+  // Live Cells:
+  //
+  // 1.  Any live cell with fewer than two live neighbours dies, as if caused by under-population.
+  // 2.  Any live cell with two or three live neighbours lives on to the next generation.
+  // 3.  Any live cell with more than three live neighbours dies, as if by overcrowding.
+  //
+  // Dead Cells
+  // 1.  Any dead cell with exactly three live neighbours becomes a live cell, as if by reproduction.
+
   step: function() {
     var i,j;
     var deltai, deltaj;
@@ -48,13 +67,21 @@ var gameoflife = {
     newCells = this.initializeNewCells();
     //console.log(newCells);
 
+// There are more efficient ways of doing this, but as long as the board is
+// a reasonable size all is good. (100 x 100 was fine).
+//
+// The more efficient ways include not having to go all the way around a cell
+// when you already went 1/2 of the way around on the previous cell.
+
     this.population = 0;
     for (i=0; i<this.cells.length; i++) {
       for (j=0; j<this.cells[i].length; j++) {
         numLiveNeighbors = 0;
+        // Start going all around -1, 0, +1 in X direction
         for (deltai=-1; deltai<=1; deltai++) {
           targeti = i+deltai;
           if (targeti >= 0 && targeti < this.cells[i].length) {  // if the i coord is on the board
+            // go all around -1, 0, +1 in the Y direction
             for (deltaj=-1; deltaj<=1; deltaj++) {
               targetj = j+deltaj;
               if ( targetj >= 0&& targetj < this.cells.length) {
@@ -63,7 +90,6 @@ var gameoflife = {
                   if (this.cells[targeti][targetj] === 1) {
                     numLiveNeighbors++;
                   }
-                  //console.log(i+","+j+" : "+targeti+","+targetj+" : "+numLiveNeighbors)                  
                 }
               }
             }
@@ -78,11 +104,11 @@ var gameoflife = {
             newCells[i][j] = 0;
           }
         } else {   // this is a dead cell
-          if (numLiveNeighbors === 3) {
+          if (numLiveNeighbors === 3) {  // new cell is born
             newCells[i][j] = 1;
           }
         }
-        if (newCells[i][j] === 1) {
+        if (newCells[i][j] === 1) {  // Tabulate the population while you're traversing
           this.population++;
         }
       }
@@ -91,10 +117,13 @@ var gameoflife = {
 
     // Swap the cells array (try to only use 2 sets of arrays by swapping them
     // rather than making a new one each cycle
+    // Saves a bit of a memory leak
+
     this.tempCells = this.cells;
     this.cells = newCells;
     this.newCells = this.tempCells;
 
+    // increment the step count
     this.numSteps++;
   },
 
